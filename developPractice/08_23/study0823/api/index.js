40005dac620291a0573b1d67f212dbc4b2e31492
chj <bluedog129@gmail.com>
2024-08-25 23:26:28 +0900

    fix : 08_23

--- developPractice/08_23/study0823/api/index.js
+++ developPractice/08_23/study0823/api/index.js
@@ -1,16 +1,18 @@
 require("dotenv").config();
-
 const express = require("express");
 const path = require("path");
 const app = express();
-const postRoutes = require("../routes/postRoutes");
+const postRoutes = require("./routes/postRoutes"); // 경로 수정
 
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-module.exports = app;
-
 // 라우터 설정
 app.use("/", postRoutes);
+
+// Vercel에서 요구하는 서버리스 함수 형태
+module.exports = (req, res) => {
+  app(req, res);
+};
